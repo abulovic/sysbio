77ac3f272a9788c78aa2147e77fef6794fbed91b
azardilis <azardilis@gmail.com>
2012-11-24 15:35:35 +0000

    addes system for positive simple harmonic oscillator with offsets from zero

--- oscillators.py
+++ oscillators.py
@@ -12,10 +12,19 @@ def dx_dt(X, t):
     y = array([X[1] , -freq**2 * X[0]])
     return y
 
+#positive simple harmonic oscillator with offsets from zero
+def ds_dt(S, t):
+    cs = 2
+    cr = 1
+    a = 1
+    b = 1
+    y = array([-cs + a**2 * S[1], cr - b**2 * S[0]])
+    return y
+
 if __name__ == '__main__':
     X0 = np.array([1, 0.5])
     t = np.linspace(0, 5 *np.pi, 100)
-    X  = integrate.odeint(dx_dt, X0, t)
+    X  = integrate.odeint(ds_dt, X0, t)
     x,y = X.T
     plt.plot(t, x, 'r-')
     plt.plot(t, y, 'b-')
