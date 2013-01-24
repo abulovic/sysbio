0f951b123c018cae380349b5723b4c9fa39dfaf6
azardilis <azardilis@gmail.com>
2013-01-24 12:41:21 +0000

    added a colMeans function similar to R

--- stats_util.py
+++ stats_util.py
@@ -7,3 +7,11 @@ def dmvnorm(x, mean, sigma):
     dev = x - mean
     part3 = np.exp(-0.5*np.dot(np.dot(dev.transpose(), np.linalg.inv(sigma)), dev))
     return part1*part2*part3
+
+
+def colMeans(X):
+    col_num = X.shape[1]
+    mean = np.zeros(col_num)
+    for i in range(col_num):
+        mean[i] = np.mean(X[:,i])
+    return mean
