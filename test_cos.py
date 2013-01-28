e4089b06ef7eebd2aa89f19b916c895c0fe1106b
azardilis <azardilis@gmail.com>
2013-01-28 10:32:10 +0000

    new tests

--- test_cos.py
+++ test_cos.py
@@ -5,10 +5,11 @@ from scipy import array
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-import abcinfer as abc
+import abcinfer_1 as abc
 import numpy as np
-times = (1.1,1.5, 2.4, 3.2, 3.9,4.5, 5.6,6.2, 7.5,8.2, 9.6,11.5, 11.9,12.5,13.5, 14.4)
-datapoints = 16
+import stats_util as utils
+times = (1.1, 2.4, 3.9, 5.6, 7.5, 9.6, 11.9,14.4)
+datapoints = 8
 
 def generate_data():
     th = 1
@@ -62,6 +63,7 @@ def plot_solution(population):
         theta.append(m)
         plt.hist(p)
         plt.figure(2)
+    print theta
     X0 = np.array([1, 0.5])
     t = np.arange(0, 15, 0.1)
     X= integrate.odeint(dx_dt, X0, t, args=(theta,))
@@ -79,22 +81,29 @@ def plot_solution(population):
     plt.show()
     
 if __name__ == "__main__":
-#    print theta
-#    n0 = [0.1,10]
-#    t = np.arange(0, 480, 5)
-#    theta1 = [0.005, 1]
-#    r = integrate.odeint(dn_dt, n0, t, args=(theta,))
-#    y = integrate.odeint(dn_dt, n0, t, args=(theta1,))
-#    plt.plot(t, r, '-o')
-#    plt.plot(t, y, 'r-')
-#    plt.legend(['Bacteria', 'Nutrients'], loc='lower right')
-#    plt.xlabel('Time')
-#    plt.ylabel('Concentration')
-#    plt.title('Simple ODE model for bacterial growth')
-#    plt.show()
     ds = generate_data()
-    population = abc.smc(dx_dt, ds, [40.0, 20.0, 16.0, 12.0])
-    plot_solution(population[len(population)-1])
+    population = abc.smc(dx_dt, ds, [30.0, 20.0, 15.0, 10.0 ,8.0])
+    last_population = population[len(population)-1]
+    plot_solution(last_population)
+    #theta = utils.colMeans(np.vstack(last_population))
+    #theta = [  7.7445538,   28.26327547, -30.11870996,   2.63604665, -14.98477143,
+    #           -13.08798084, -24.30999597,   7.69635589]
+    
+    #X0 = np.array([1, 0.5])
+    #t = np.arange(0, 15, 0.1)
+    #X= integrate.odeint(dx_dt, X0, t, args=(theta,))
+    #x,y = X.T
+    #plt.figure(1)
+    #plt.subplot(211)
+    #plt.plot(t, x, 'r-', label='x(t)')
+    #plt.plot(t,np.cos(t),'g-',label='x(t))')
+    #plt.subplot(212)
+    #plt.plot(t, y, 'b-', label='y(t)')
+    #plt.plot(t, np.cos(t+1), 'g-', label='y(t)')
+    #plt.xlabel('time')
+    #plt.show()
+    
+    
     
 
     
