c72ed47f22460968f971646623f6710d8b31bdc0
azardilis <azardilis@gmail.com>
2013-02-13 17:01:30 +0000

    changed the names to the new ones

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
  
-ext_modules = [Extension("abcinfer_1", ["abcinfer_1.py"])]
+ext_modules = [Extension("abcinfer_multi_thr", ["abcinfer_multi_thr.py"])]
  
 setup(
   name = 'ABC inference',
